d770412df01d5f7f6ded47a7e3890a75eb40e3e2
joweid <johannes-weidmann@web.de>
2023-06-16 10:56:51 +0200

    build graph in GUI

--- src/Cycle_Canceling.cpp
+++ src/Cycle_Canceling.cpp
@@ -2,28 +2,104 @@
  *	Studien_Projekt
  *
  *      Das Minimum Cost Flow Problem
- *      
+ *
  *      Visualisierung mit dem Cycle Canceling Algorithmus
  *
  * ************************/
 
 #include <LEDA/graph/graph.h>
-#include <LEDA/core/queue.h>
 #include <LEDA/graphics/graphwin.h>
 
 using namespace leda;
 
+// Variables
+const color cost_c = red;
+const color cap_c = blue;
+
+static GRAPH<int,int> G;
+
+static edge_map<int> Gcost(G);
+static edge_map<int> Gcap(G);
+
+// Initialize Edge Handler
+void init_edge(GraphWin& gw, edge e){
+    Gcap[e] = rand_int(10,20);
+    Gcost[e] = rand_int(30,50);
+    gw.set_width(e,Gcap[e]/10+5);
+    gw.set_slider_value(e,Gcap[e]/100.0,0);
+    gw.set_slider_value(e,Gcost[e]/100.0,1);
+    gw.set_label(e,string("cost = %d \n cap = %d",Gcost[e],Gcap[e]));
+}
+
+void new_edge_handler(GraphWin& gw, edge e){
+    init_edge(gw,e);
+}
+
+// Initialize Node Handler
+void init_node(GraphWin& gw, node n){
+    gw.set_label_type(n, gw_label_type::data_label);
+}
+
+void new_node_handler(GraphWin& gw, node n){
+    init_node(gw, n);
+}
+
+// Cost Handler & Slider
+void cost_slider_handler(GraphWin& gw, edge e, double f){
+    Gcost[e] = int(100*f);
+    gw.set_label(e,string("cost = %d \n cap = %d",Gcost[e],Gcap[e]));
+}
+
+// Cap Handler & Slider
+void cap_slider_handler(GraphWin& gw,edge e, double f){
+    Gcap[e] = int(100*f);
+    gw.set_width(e,Gcap[e]/10+5);
+    gw.set_label(e,string("cost = %d \n cap = %d",Gcost[e],Gcap[e]));
+}
+
 int main(){
-	// Creating graph window and setting title
-	GraphWin gw("Cycle Canceling");
-	graph& G = gw.get_graph();
-	gw.display(window::center, window::center); 
+    node v;
+    // Creating Graph Window
+    GraphWin gw(G,"Cycle Canceling Algorithm");
+    gw.display(window::center, window::center);
+    gw.set_directed(true);
 
-	// While graph is edited, do stuff
-	while ( gw.edit() ){
+    // Setting Handlers
+    gw.set_new_edge_handler(new_edge_handler);
+    gw.set_new_node_handler(new_node_handler);
 
-	}
-	return 0; 
+    gw.set_edge_slider_handler(cost_slider_handler,1);
+    gw.set_edge_slider_color(cost_c,1);
+
+    gw.set_edge_slider_handler(cap_slider_handler,0);
+    gw.set_edge_slider_color(cap_c,0);
+
+    while(gw.edit()){
+        int balance = 0;
+        forall_nodes(v,G) {
+            if (G.node_data()[v] > 0) {
+                gw.set_color(v, green);
+            }
+            else if (G.node_data()[v] < 0) {
+                gw.set_color(v, red);
+            }
+            else {
+                gw.set_color(v, grey1);
+            }
+        }
+
+        forall_nodes(v,G) {
+            balance += G.node_data()[v];
+        }
+        if (balance != 0) {
+            gw.message("Supply/Demand values don't sum up to zero.");
+        }
+        else {
+            gw.message("Correct Supply Values.");
+        }
+    }
+
+    return 0;
 }
 
 
