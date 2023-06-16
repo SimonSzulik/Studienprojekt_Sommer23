efe81da5a6a9231fd37614d3296a03a274153de5
Simon Szulik <s4siszul@uni-trier.de>
2023-06-17 00:14:20 +0200

    Slider Werte sind jetzt neben Edge. Dicke des Pfeils passt sich an Capacity an. Unnötige Slider entfernt.

--- src/Succesive_Shortest_Path.cpp
+++ src/Succesive_Shortest_Path.cpp
@@ -12,6 +12,7 @@
 
 using namespace leda;
 
+// Variables
 const color cost_c = red;
 const color cap_c = blue;
 
@@ -20,51 +21,57 @@ static GRAPH<int,int> G;
 static edge_map<int> Gcost(G);
 static edge_map<int> Gcap(G);
 
-void init_edge(GraphWin& gw, edge e)
-{ Gcap[e] = rand_int(10,50);
-    Gcost[e] = rand_int(10,75);
+// Initialize Edge Handler
+void init_edge(GraphWin& gw, edge e){
+    Gcap[e] = rand_int(10,20);
+    Gcost[e] = rand_int(30,50);
+    gw.set_width(e,Gcap[e]/10+5);
     gw.set_slider_value(e,Gcap[e]/100.0,0);
     gw.set_slider_value(e,Gcost[e]/100.0,1);
+    gw.set_label(e,string("cost = %d \n cap = %d",Gcost[e],Gcap[e]));
 }
 
-void new_edge_handler(GraphWin& gw, edge e)
-{ init_edge(gw,e);
+void new_edge_handler(GraphWin& gw, edge e){
+    init_edge(gw,e);
 }
 
-void start_cost_slider_handler(GraphWin& gw, edge, double)
-{ gw.message("\\bf\\red Change Edge Cost"); }
-
-void cost_slider_handler(GraphWin& gw, edge e, double f)
-{ Gcost[e] = int(100*f);
-    gw.set_label_color(e,cost_c);
-    gw.set_label(e,string("cost = %d",Gcost[e]));
+// Initialize Node Handler
+void init_node(GraphWin& gw, node n){
+    gw.set_label(n, "0");
 }
 
-void start_cap_slider_handler(GraphWin& gw, edge, double)
-{ gw.message("\\bf\\blue Change Edge Capacity"); }
+void new_node_handler(GraphWin& gw, node n){
+    init_node(gw, n);
+}
 
-void cap_slider_handler(GraphWin& gw,edge e, double f)
-{ Gcap[e] = int(100*f);
-    gw.set_label_color(e,cap_c);
-    gw.set_label(e,string("cap = %d",Gcap[e]));
+// Cost Handler & Slider
+void cost_slider_handler(GraphWin& gw, edge e, double f){
+    Gcost[e] = int(100*f);
+    gw.set_label(e,string("cost = %d \n cap = %d",Gcost[e],Gcap[e]));
+}
+
+// Cap Handler & Slider
+void cap_slider_handler(GraphWin& gw,edge e, double f){
+    Gcap[e] = int(100*f);
+    gw.set_width(e,Gcap[e]/10+5);
+    gw.set_label(e,string("cost = %d \n cap = %d",Gcost[e],Gcap[e]));
 }
 
 int main(){
-	// Creating graph window and setting title
+	// Creating Graph Window
 	GraphWin gw(G,"Successive Shortest Path Algorithm");
 	gw.display(window::center, window::center);
+    gw.set_directed(true);
 
+    // Setting Handlers
     gw.set_new_edge_handler(new_edge_handler);
+    gw.set_new_node_handler(new_node_handler);
 
-    gw.set_start_edge_slider_handler(start_cost_slider_handler,0);
     gw.set_edge_slider_handler(cost_slider_handler,1);
     gw.set_edge_slider_color(cost_c,1);
 
-    gw.set_start_edge_slider_handler(start_cap_slider_handler,0);
     gw.set_edge_slider_handler(cap_slider_handler,0);
     gw.set_edge_slider_color(cap_c,0);
-
-    gw.set_directed(true);
 
     while(gw.edit()){
 
